3444dacb0f7370c7c09628301d632496690fcd11
Aissam Elboudi <issam.rma10@gmail.com>
2022-04-04 14:08:54 +0000

    login.js -> add login list script

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const { jwt } = require('../../lib/jwt')
 const { logInClass } = require('../../models/models')
+const dateTime = require('node-datetime')
+const mariadb = require('../../lib/mariadb').mariadb
 
 /*endpoint: /login, methode: POST */
 router.post('/', async (req, res) => {
 
-    const pool = require('../../lib/mariadb').pool
     const data = req.body
-
     switch (true) {
         case (data.username == undefined): {
             res.json({
@@ -25,47 +25,63 @@ router.post('/', async (req, res) => {
             return
         }
     }
+    // promisse mariadb
+    mariadb.then((pool) => {
+        const username = data.username
+        const password = data.password
+        const login = new logInClass(username, password)
+        const command = 'SELECT * FROM login_table WHERE username=? AND password=?'
+        /*query user with username and password*/
+        pool.query(command, [login.username, login.password]).then((resSearch) => {
 
-    const username = data.username
-    const password = data.password
-    const login = new logIn(username, password)
-    const command = 'SELECT * FROM login_table WHERE username=? AND password=?'
-    /*query user with username and password*/
-    pool.query(command, [login.username, login.password]).then((resSearch) => {
-
-        // in case no user has info match username and password
-        if (resSearch[0] == undefined) {
-            res.json({
-                success: false,
-                message: 'Valid email or password '
-            })
-            return
-        }
-        // generate token for user
-        jwt.jwtSign({
-            username: login.username,
-            password: login.password
-        }).then((result) => {
-            const commandUpdateToken = 'UPDATE login_table SET token=? WHERE username=?'
-            pool.query(commandUpdateToken, [result.token, login.username]).then((resQuery) => {
+            // in case no user has info match username and password
+            if (resSearch[0] == undefined) {
                 res.json({
-                    token: result.token,
-                    success: true,
-                    user: {
-                        username: login.username,
-                    },
+                    success: false,
+                    message: 'Valid email or password '
                 })
+                return
+            }
+            // generate token for user
+            jwt.jwtSign({
+                username: login.username,
+                password: login.password
+            }).then((result) => {
+                const commandUpdateToken = 'UPDATE login_table SET token=? WHERE username=?'
+                pool.query(commandUpdateToken, [result.token, login.username]).then((resQuery) => {
+                    res.json({
+                        token: result.token,
+                        success: true,
+                        user: {
+                            username: login.username,
+                        },
+                    })
+                }).catch((err) => {
+                    res.json({
+                        success: false,
+                        message: err
+                    })
+                })
+
+                //command insert any event login
+                const commandLoginList = 'INSERT INTO login_list_table(token, username, password, login_time) VALUES(?,?,?,?)'
+                /*create createdDate for notification */
+                const time = dateTime.create()
+                const formatted = time.format('y-m-d H:M:S')
+                /* format() funtions return format 22-04-01 14:55 but require 2022-04-01 14:55 */
+                const login_time = '20' + formatted
+                pool.query(commandLoginList, [result.token, login.username, login.password, login_time])
             }).catch((err) => {
                 res.json({
                     success: false,
                     message: err
                 })
             })
-        }).catch((err) => {
-            res.json({
-                success: false,
-                message: err
-            })
+        })
+    }).catch((err) => {
+        res.json({
+            success: false,
+            message: err
         })
     })
 })
